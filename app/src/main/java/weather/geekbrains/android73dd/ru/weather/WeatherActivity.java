7dbec92225c816bd05f0f068fdfa69fe7b497887
Evgeny Uleykin <euleykin@getmobit.ru>
2018-05-14 16:26:39 +0300

    first commit

---
+++ app/src/main/java/weather/geekbrains/android73dd/ru/weather/WeatherActivity.java
@@ -0,0 +1,13 @@
+package weather.geekbrains.android73dd.ru.weather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
