1ffca785d5b7950e853fd977b426e12e37323235
Evgeny Uleykin <euleykin@getmobit.ru>
2018-06-18 17:38:34 +0300

    add navigation drawer

--- app/src/main/java/ru/android73dd/geek/weather/ui/MainActivity.java
+++ app/src/main/java/ru/android73dd/geek/weather/ui/MainActivity.java
@@ -1,8 +1,14 @@
 package ru.android73dd.geek.weather.ui;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -10,15 +16,27 @@ import android.view.MenuItem;
 
 import ru.android73dd.geek.weather.R;
 
-public class MainActivity extends AppCompatActivity implements CitiesFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, CitiesFragment.OnFragmentInteractionListener {
+
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_drawer);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.activity_main_drawer);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         CitiesFragment citiesFragment = CitiesFragment.newInstance();
         getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, citiesFragment).commit();
@@ -43,5 +61,30 @@ public class MainActivity extends AppCompatActivity implements CitiesFragment.On
     @Override
     public void onItemClicked(String cityName) {
         startActivity(WeatherDetailsActivity.getIntent(this, cityName));
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_stub:
+                break;
+            case R.id.nav_about_developer:
+                break;
+            case R.id.nav_site:
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+                startActivity(browserIntent);
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
